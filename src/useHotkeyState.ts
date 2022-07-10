e2881f97923ec2a692e19227def18e19cb4a0cc3
Gabin Aureche <hello@gabinaureche.com>
2022-07-10 21:13:06 +0200

    Keep track of the mousetrap instances

--- src/useHotkeyState.ts
+++ src/useHotkeyState.ts
@@ -1,5 +1,9 @@
 import { RefObject, useEffect, useState } from 'react';
-import Mousetrap, { ExtendedKeyboardEvent } from 'mousetrap';
+import Mousetrap, {
+  ExtendedKeyboardEvent,
+  MousetrapInstance,
+  MousetrapStatic,
+} from 'mousetrap';
 
 export type HotkeyShortcuts = {
   name: string;
@@ -17,22 +21,62 @@ export type HotkeyShortcuts = {
  * Creates a global state singleton.
  */
 const createStateHook = () => {
+  const mousetraps = new Map<
+    HTMLElement | undefined,
+    MousetrapStatic | MousetrapInstance
+  >();
   let keys: HotkeyShortcuts[] = [];
 
   const addKeys = (nextKeys: HotkeyShortcuts[]) => {
     keys = [...keys, ...nextKeys];
 
     nextKeys.forEach((k) => {
-      if (!k.disabled) {
-        const mousetrap = k.ref?.current ? Mousetrap(k.ref.current) : Mousetrap;
-        mousetrap.bind(k.keys, k.callback, k.action);
+      if (k.ref && !k.ref.current) {
+        // exit early if ref is provided but null
+        // we do not want to attach global event handlers in this case
+        return;
       }
+
+      if (k.disabled) {
+        return;
+      }
+
+      const element = k.ref?.current ?? undefined;
+
+      if (!mousetraps.has(element)) {
+        mousetraps.set(element, new Mousetrap(element));
+      }
+
+      const mousetrap = mousetraps.get(element)!;
+      mousetrap.bind(k.keys, k.callback, k.action);
     });
   };
 
   const removeKeys = (nextKeys: HotkeyShortcuts[]) => {
+    // remove keys from the array
     keys = keys.filter((k) => !nextKeys.includes(k));
-    nextKeys.forEach((s) => Mousetrap.unbind(s.keys, s.action));
+
+    // unbind mousetrap events
+    nextKeys.forEach((k) => {
+      const element = k.ref?.current ?? undefined;
+      const mousetrap = mousetraps.get(element)!;
+      mousetrap.unbind(k.keys, k.action);
+    });
+
+    // drop mousetrap instances that became unused
+    for (const [element] of mousetraps) {
+      if (
+        keys.some((k) => {
+          const kElement = k.ref ? k.ref.current : undefined;
+          return kElement === element;
+        })
+      ) {
+        // do not drop if there's at least one key that's still relying on this instance
+        continue;
+      }
+
+      mousetraps.delete(element);
+    }
   };
 
   return () => {
